cf1c049803c42c4def3a3f64ec4c99c6a1203be1
arthurpeuvot <arthur.peuvot@epfedu.fr>
2022-11-29 16:10:15 +0100

    forth commit

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
 <script async
-src="https://www.googletagmanager.com/gtag/js?id=344254954"></script>
+src="https://www.googletagmanager.com/gtag/js?id=G-3SCJG24T32"></script>
 <script>
  window.dataLayer = window.dataLayer || [];
  function gtag(){dataLayer.push(arguments);}
  gtag('js', new Date());
- gtag('config', '344254954');
+ gtag('config', 'G-3SCJG24T32');
 </script>
  """
  return prefix_google + "Hello World"
