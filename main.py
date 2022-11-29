720d41a4904a47dc026406c713f74234f334e730
arthurpeuvot <arthur.peuvot@epfedu.fr>
2022-11-29 15:59:12 +0100

    third commit

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', methods=["GET"])
 def hello_world():
-    return "Hello World"
+ prefix_google = """
+ <!-- Google tag (gtag.js) -->
+<script async
+src="https://www.googletagmanager.com/gtag/js?id=344254954"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+ gtag('config', '344254954');
+</script>
+ """
+ return prefix_google + "Hello World"
