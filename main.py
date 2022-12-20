843b4a299538948c1a8397b60e25ae28d1b1dfba
arthurpeuvot <arthur.peuvot@epfedu.fr>
2022-12-20 18:48:03 +0100

    logger

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request, Response, render_template
+import logging
+import sys
+from io import StringIO
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
 
+# Create a stream-like object using StringIO
+stream = StringIO()
+
+# Configure a stream handler for the logger
+stream_handler = logging.StreamHandler(stream)
+stream_handler.setLevel(logging.WARNING)
+
+# Add the handler to the logger's handlers list
+app.logger.addHandler(stream_handler)
+
+@app.route('/', methods=["GET"])
 def hello_world():
  prefix_google = """
  <!-- Google tag (gtag.js) -->
@@ -17,3 +30,14 @@ src="https://www.googletagmanager.com/gtag/js?id=G-3SCJG24T32"></script>
 </script>
  """
  return prefix_google + "Hello World"
+
+@app.route("/logger", methods=["GET", "POST"])
+def logger():
+    app.logger.warning('A warning occurred !!')
+    log_messages = stream_handler.stream.getvalue()
+    print(log_messages) # it can be seen on Deta 
+    return log_messages
+
+
+if __name__=="__main__":
+    app.run(debug=True)
