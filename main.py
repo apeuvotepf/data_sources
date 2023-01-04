6358cfabc9681b7fef40cecb27095ef9facbd98f
arthurpeuvot <arthur.peuvot@epfedu.fr>
2023-01-04 16:33:59 +0100

    google trend

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from flask import Flask, request, Response, render_template
 import logging
 from io import StringIO
 import requests
+from oauth2client.service_account import ServiceAccountCredentials
+from pytrends.request import TrendReq
+import pandas as pd
+from datetime import datetime
+import random
 
 app = Flask(__name__)
+
+
+SCOPES = ['https://www.googleapis.com/auth/analytics.readonly']
+KEY_FILE_LOCATION = 'data-sources-372218-e099443d9007.json'
+VIEW_ID = '<REPLACE_WITH_VIEW_ID>'
 
 # Create a stream-like object using StringIO
 stream = StringIO()
@@ -37,6 +47,7 @@ def logger():
     print(log_messages) # it can be seen on Deta 
     return render_template('logger.html', log_messages=log_messages)
 
+
 @app.route("/cookies", methods=["GET", "POST"])
 def cookies():
     # Make a GET request to Google
@@ -47,6 +58,83 @@ def cookies():
     # Display the cookies in the app
     # return str(cookies)
     return req.text
+
+
+@app.route('/trends', methods=['GET', 'POST'])
+def trends():
+    if request.method == 'POST':
+
+        # Initialize the pytrends library
+        pytrend = TrendReq()
+
+        # Get the keywords from the form
+        keywords = request.form['keywords']
+
+        # Split the keywords into a list
+        keywords_list = keywords.split(' ')
+
+        # Set the timeframe for the trend data
+        timeframe='2022-01-01 2023-01-01'
+
+        # Get the trend data
+        pytrend.build_payload(kw_list=keywords_list, timeframe=timeframe)
+        trend_data = pytrend.interest_over_time().drop(['isPartial'], axis=1)
+
+        # Convert the trend data to a Pandas DataFrame
+        dates = [datetime.fromtimestamp(int(date/1e9)).date().isoformat() for date in trend_data.index.values.tolist()]
+
+        # Convert the dataframe to a list of dictionaries for the chart data
+        params = {'type': 'line', 
+                'data':{
+                    'labels': dates,
+                    'datasets':[]
+                },
+                'options': {
+                    "title": {
+                    "text": 'My chart'
+                },
+                "scales": {
+                    "yAxes": [{
+                        "ticks": {
+                            "beginAtZero": 'true'
+                        }
+                    }]
+                }
+                }}
+
+        for column in trend_data.columns:
+            params['data']['datasets'].append({'label': column, 
+                                            'data': trend_data[column].values.tolist(),
+                                            # generate random color
+                                            "borderColor": "#"+''.join([random.choice('0123456789ABCDEF') for i in range(6)]),
+                                            "fill": 'false',
+                                        })
+
+
+        template = f"""  
+        <html>
+            <head>
+                <title>Trend Comparison</title>
+            </head>
+            <body>
+                <h1>Trend Data</h1>
+                <p>Keywords: {keywords}</p>    
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+                <div><canvas id="myChart" width="25" height="25"></canvas></div>
+                <script>
+                    var ctx = document.getElementById('myChart');
+                    var myChart = new Chart(ctx, {params});
+                </script>
+            </body>
+        </html>
+        """
+        
+        return template
+    else:
+        # Render the trends form template
+        return render_template('trends_form.html')
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
